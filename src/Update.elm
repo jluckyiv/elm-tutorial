c3fca111ec3e64e39baa9ba7348a3657426c07aa
Jack Lucky <jack.lucky.iv@gmail.com>
2017-03-12 09:59:33 -0700

    Handle player edit messages in update

--- src/Update.elm
+++ src/Update.elm
@@ -1,13 +1,22 @@
 module Update exposing (..)
 
+import Commands exposing (savePlayerCmd)
 import Msgs exposing (Msg)
-import Models exposing (Model)
+import Models exposing (Model, Player)
 import Routing exposing (parseLocation)
+import RemoteData
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        Msgs.ChangeLevel player howMuch ->
+            let
+                updatedPlayer =
+                    { player | level = player.level + howMuch }
+            in
+                ( model, savePlayerCmd updatedPlayer )
+
         Msgs.OnFetchPlayers response ->
             ( { model | players = response }, Cmd.none )
 
@@ -17,3 +26,27 @@ update msg model =
                     parseLocation location
             in
                 ( { model | route = newRoute }, Cmd.none )
+
+        Msgs.OnPlayerSave (Ok player) ->
+            ( updatePlayer model player, Cmd.none )
+
+        Msgs.OnPlayerSave (Err error) ->
+            ( model, Cmd.none )
+
+
+updatePlayer : Model -> Player -> Model
+updatePlayer model updatedPlayer =
+    let
+        pick currentPlayer =
+            if updatedPlayer.id == currentPlayer.id then
+                updatedPlayer
+            else
+                currentPlayer
+
+        updatePlayerList players =
+            List.map pick players
+
+        updatedPlayers =
+            RemoteData.map updatePlayerList model.players
+    in
+        { model | players = updatedPlayers }
